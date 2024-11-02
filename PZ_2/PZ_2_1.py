45e0fd2067a3b5dbc0c606261cfa83a32ae90925
RostislavKozyayev <rossttiislav@gmail.com>
2024-11-02 16:20:50 +0300

    Изменил ПЗ2

--- PZ_2/PZ_2_1.py
+++ PZ_2/PZ_2_1.py
@@ -1,5 +1,16 @@
-number = int(input("Введитее трехзначное число: "))
-last_digit = number % 10
-middle_digit = (number // 10) % 10
-print(last_digit)
-print(middle_digit)
+#Дано трехзначное число. Вывести вначале его последнюю цифру(единицы), а затем — его среднюю цифру (десятки).
+
+#проверка
+try:
+
+    #ввод
+    number = int(input("Введите трехзначное число: "))
+
+    #нахождение
+    last_digit: int = number % 10
+    middle_digit = (number // 10) % 10
+    print(last_digit)
+    print(middle_digit)
+
+except ValueError:
+    print("что-то пошло не так")
